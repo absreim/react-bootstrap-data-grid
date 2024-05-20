75d62345862a022a3852682d0336b2cb9cd26ec4
Brook Li <emcdynamicist@gmail.com>
2024-05-20 19:00:31 -0400

    Added types for filter control states for different column types

--- src/grid/types.ts
+++ src/grid/types.ts
@@ -34,3 +34,39 @@ export interface TableSortModel {
   sortColDef: SortColDef | null;
   setSortColDef: (sortColDef: SortColDef | null) => void;
 }
+
+export interface FilterState {
+  enabled: boolean;
+}
+export type StringFilterScheme = "contains" | "startsWith" | "endsWith";
+export interface StringFilterState extends FilterState {
+  scheme: StringFilterScheme;
+  searchString: string;
+}
+export type NumberFilterScheme =
+  | "equals"
+  | "greaterThan"
+  | "lessThan"
+  | "greaterOrEqual"
+  | "lessOrEqual";
+export interface NumberFilterState extends FilterState {
+  scheme: NumberFilterScheme;
+  numInputValue: string;
+}
+export type DateFilterScheme = "startFrom" | "endAt" | "between";
+export interface DateFilterState extends FilterState {
+  scheme: DateFilterScheme;
+}
+export interface StartDateFilterState extends DateFilterState {
+  scheme: "startFrom";
+  startDate: Date;
+}
+export interface EndDateFilterState extends DateFilterState {
+  scheme: "endAt";
+  endDate: Date;
+}
+export interface BetweenDatesFilterState extends DateFilterState {
+  scheme: "between";
+  startDate: Date;
+  endDate: Date;
+}
