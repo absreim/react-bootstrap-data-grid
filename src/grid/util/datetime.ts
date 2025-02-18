0c1de45a40049e9deacfd41cd6c8cee7fd31340c
Brook Li <emcdynamicist@gmail.com>
2025-02-17 19:42:33 -0500

    Implemented form filter model to filter model converter

--- src/grid/util/datetime.ts
+++ src/grid/util/datetime.ts
@@ -7,3 +7,8 @@ export const dateToInputStr = (date: Date) => (
 export const dateToDatetimeInputStr = (date: Date) => (
   dayjs(date).format("YYYY-MM-DDTHH:mm")
 )
+
+// All dates and datetimes that the grid displays are in UTC. The datetime
+// string in the value attribute of a input element of type datetime-local
+// is based on the local timezone of the client.
+export const datetimeInputStrToUtc: (datetimeStr: string) => string = (datetimeStr) => datetimeStr + "Z"
