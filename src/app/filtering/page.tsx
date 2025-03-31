fdb85af3eb37e6d257d7962fb3da9b853909fc69
Brook Li <emcdynamicist@gmail.com>
2025-03-30 20:26:41 -0400

    Started adding sample filtered grid and encountered bugs with filtering

---
+++ src/app/filtering/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import SampleFilteredGrid from "@/app/filtering/SampleFilteredGrid";
+
+const Filtering: FC = () => {
+  return (
+    <>
+      <h1>Filtering</h1>
+      <SampleFilteredGrid />
+    </>
+  )
+}
+
+export default Filtering;
