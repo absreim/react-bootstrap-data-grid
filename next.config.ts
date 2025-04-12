4efb3a7a60ca5e09b8b9d2c0a6f53fc4cc2ad5b3
Brook Li <emcdynamicist@gmail.com>
2025-04-12 19:04:48 -0400

    Upgraded NextJS to 15 and silenced Sass import warning.

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+    // TODO: keep an eye out for updated implementations bootstrap and highlight.js that do not use the deprecated
+    // @import feature of Dart Sass
+    // References: https://github.com/twbs/bootstrap/issues/40962,
+    // https://sass-lang.com/blog/import-is-deprecated/
+  }
+}
+
+export default nextConfig;
