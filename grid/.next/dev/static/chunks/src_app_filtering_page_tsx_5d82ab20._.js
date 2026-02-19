bdffbaa036beaed24b0cc81802aaf25993ea1152
Brook Li <emcdynamicist@gmail.com>
2026-02-19 16:09:04 +0900

    fixed some minor bugs

--- grid/.next/dev/static/chunks/src_app_filtering_page_tsx_5d82ab20._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_8f98411c._.js",
-  "static/chunks/node_modules_0b9b9eed._.js"
-],
-    source: "dynamic"
-});
--- grid/.next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_global_scss_c2d3905b.css"
-],
-    source: "dynamic"
-});
