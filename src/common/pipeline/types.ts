e4db00d9aacee42fff252728aefdf5b9c9f457c4
Brook Li <emcdynamicist@gmail.com>
2026-09-13 21:03:29 +0900

    Wrote selection feature TSDoc comments

--- src/common/pipeline/types.ts
+++ src/common/pipeline/types.ts
@@ -1,3 +1,8 @@
+/**
+ * A hash map that associates column name to a width setting.
+ *
+ * @internal
+ */
 export type ColNameToWidth<ColName extends string = string> = Record<
   ColName,
   number | undefined
